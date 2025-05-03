8f7f5aa0f6c0ee7396622d68e37edf7f6240d38e
yagof <yagofer12@gmail.com>
2025-05-02 22:14:15 -0300

    exponential_search

---
+++ dsa/exponential_search.go
@@ -0,0 +1,31 @@
+package dsa
+
+func exponentialSearch(nums []int, target int) int {
+	// se for o primeiro
+	if nums[0] == target {
+		return 0
+	}
+
+	var arrSize int = len(nums)
+	var rightIndex int = 1
+
+	// dobra a busca pelo index
+	for rightIndex < arrSize && nums[rightIndex] < target {
+		rightIndex *= 2
+	}
+
+	startSubArr := rightIndex / 2
+	endSubArr := rightIndex
+
+	if endSubArr >= arrSize {
+		endSubArr = arrSize
+	}
+
+	// se for o ultimo
+	if nums[rightIndex] == target {
+		return rightIndex
+	}
+
+	// maybe i can use binary search here, but...
+	return search(nums[startSubArr:endSubArr], target)
+}
