ed1577362ee92a32c07a90c8158449ad33afebd0
yagof <yagofer12@gmail.com>
2025-06-01 12:45:40 -0300

    first unique char

--- structure/first_unique_char.go
+++ structure/first_unique_char.go
@@ -1,5 +1,22 @@
 package structure
 
 func FirstUniqueChar(s string) int {
-	return 0
+	var hashMap = map[string][]int{}
+
+	for idx, ch := range s {
+		if _, existe := hashMap[string(ch)]; !existe {
+			hashMap[string(ch)] = []int{idx, 1}
+		} else {
+			hashMap[string(ch)][1] += 1
+		}
+	}
+
+	// idx _
+	for _, hashValue := range hashMap {
+		if hashValue[1] == 1 {
+			return hashValue[0]
+		}
+	}
+
+	return -1
 }
