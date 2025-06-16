82a8a664995b6545bd25f75ba9553c8a303121fd
yagof <yagofer12@gmail.com>
2025-06-16 11:47:42 -0300

    two sum

--- structure/two_sum.go
+++ structure/two_sum.go
@@ -1,5 +1,17 @@
 package structure
 
 func twoSum(nums []int, target int) []int {
+	var hashMap = map[int]int{}
+
+	for i, num := range nums {
+		for idxHash, hashValue := range hashMap {
+			if hashValue+num == target {
+				nums = []int{idxHash, i}
+			}
+		}
+
+		hashMap[i] = num
+	}
+
 	return nums
 }
